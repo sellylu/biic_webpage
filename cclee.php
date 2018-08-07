5b6c7ee50a12f21ceb0e61c90f85cba2969feb74
sellylu <lu721@gapp.nthu.edu.tw>
2018-08-07 18:05:54 +0800

    add page 'professor'

---
+++ cclee.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta name="Description" content="Behavioral Informatics & Interaction Computation Lab(BIIC)">
+	<meta name="KeyWords" content="biic,nthu,ee,chih-chun lee,bsp,emotion,清大電機,人類行為訊息,互動計算,李祈均">
+	<title>BIIC Lab</title>
+
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="no-sidebar is-preload">
+	<div id="page-wrapper">
+
+		<?php include "./header.php";?>
+		</section>
+
+		<!-- Main -->
+		<section id="main">
+			<div class="container">
+
+				<!-- Content -->
+				<article class="box post">
+					<a href="#" class="image featured"><img src="images/jeremy.jpg" alt="" /></a>
+					<header>
+						<h2>Chi-Chun (Jeremy) Lee</h2>
+						<p>
+						Associate Professor of Electrical Engineering<br/>
+						National Tsing Hua University (NTHU)
+						</p>
+						<div>
+						<b>Office:</b>&emsp;&emsp;Room 868, Delta Building<br/>
+						<b>Email:</b>&emsp;&emsp;<a href="mailto: cclee@ee.nthu.edu.tw">cclee@ee.nthu.edu.tw</a><br/>
+						<b>Address:</b>&emsp;No. 101, Section 2, Kuang-Fu Road, 30013 Hsinchu City, Taiwan(R.O.C.)<br/>
+						</div>
+					</header>
+					<p>
+						Chi-Chun Lee (Jeremy) is an Associate Professor at the Electrical Engineering Department of the National Tsing Hua University (NTHU), Taiwan. He received his B.S. degree and Ph.D. degree both in Electrical Engineering from the University of Southern California, USA in 2007 and 2012. He was a data scientist at id:a lab at ID Analytics in 2013. His research interests are in the human-centered behavioral signal processing (BSP) and affective computing. He was awarded with the USC Annenberg Fellowship (2007 -2009). He led a team to participate and win the Emotion Challenge Classifierer Sub-Challenge in Interspeech 2009. He is a coauthor on the best paper award in Interspeech 2010, and the most cited paper published in 2013 in Journal of Speech Communication on automatic modeling of couples' behaviors during therapeutic sessions. He is a member of Tau Beta Pi, Phi Kappa Phi and Eta Kappa Nu. He is also a member of IEEE and ISCA. He has been a reviewer for multiple internationally-renowned journals and program committee for international technical conferences. He recently served as an area chair for Interspeech 2016 and 2018, senior program committee for ACII 2017, and a guest editor in Journal of Computer Speech and Language on special issue of Speech and Language Processing for Behavioral and Mental Health. He has been involved in multiple granted interdisciplinary research projects, including aspects on education, psychology, neuroscientific, and health-related applications, with a focus of modeling human behaviors using signal processing and machine learning.
+					</p>
+					<section>
+						<!-- <header>
+							<h3>Something else</h3>
+						</header> -->
+						<p class="personal-des"><a title="Google Scholar - Jeremy" href="http://scholar.google.com.tw/citations?user=MGqWqOAAAAAJ&hl=en" target="_blank">Google Scholar Profile</a></p>
+						<p class="personal-des""><a href="./Jeremy_CV.pdf" target="_blank">Jeremy's CV</a>&emsp;(update: Dec. 2017)</p>
+						<p class="personal-des"><a title="Interdisciplinary Insights into Group and Team Dynamics" href="https://www.4tu.nl/ht/en/news/%21/112600/workshop-group-dynamics/" target="_blank">Workshop on Interdisciplinary Insights into Group and Team Dynamics</a></p>
+					</section>
+				</article>
+
+			</div>
+		</section>
+
+		<?php include "./footer.php";?>
+
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.dropotron.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
